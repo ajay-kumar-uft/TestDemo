fb433b5962f1a5bb89907bb259f0c382639dcba7
Ajay Kumar <ajay.kumar@urbanft.com>
2024-01-17 23:19:56 +0530

    Code updated

---
+++ docscan/src/main/java/com/contourdocumentimaging/android/contours_ai/util/ContoursPreference.kt
@@ -0,0 +1,112 @@
+package com.contourdocumentimaging.android.contours_ai.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class ContoursPreference private constructor(context: Context) {
+
+    private val CONTOURS_PREFERENCES = "ContoursPreference"
+    private var sharedPreferences: SharedPreferences? = null
+
+    fun shouldShowIntroScreen(screenNameKey: String): Boolean {
+        return sharedPreferences!!.getBoolean(screenNameKey, true)
+    }
+
+    fun setIntroScreenVisibility(screenNameKey: String, value: Boolean? = true) {
+        sharedPreferences!!.edit().putBoolean(screenNameKey, value?:true).apply()
+    }
+
+    fun setAESKey(hex: String) {
+        sharedPreferences!!.edit().putString(AES_KEY, hex).apply()
+    }
+
+    fun getAESKey(): String {
+        return sharedPreferences!!.getString(AES_KEY, "") ?: ""
+    }
+
+    fun setDeviceId(deviceId: String) {
+        sharedPreferences!!.edit().putString(DEVICE_ID, deviceId).apply()
+    }
+
+    fun getDeviceId(): String {
+        return sharedPreferences!!.getString(DEVICE_ID, "") ?: ""
+    }
+
+    fun setVersionNo(versionNo: String) {
+        sharedPreferences!!.edit().putString(VERSION_NO, versionNo).apply()
+    }
+
+    fun getVersionNo(): String {
+        return sharedPreferences!!.getString(VERSION_NO, "") ?: ""
+    }
+
+    fun setCountOfflineValidation(count: Int) {
+        sharedPreferences!!.edit().putInt(COUNT_OFFLINE_VALIDATION, count).apply()
+    }
+
+    fun getCountOfflineValidation(): Int {
+        return sharedPreferences!!.getInt(COUNT_OFFLINE_VALIDATION, 0) ?: 0
+    }
+
+    fun setContentLengthWeightFile(contentLength: Int) {
+        sharedPreferences!!.edit().putInt(CONTENT_LENGTH_WEIGHT_FILE, contentLength).apply()
+    }
+
+    fun getContentLengthWeightFile(): Int {
+        return sharedPreferences!!.getInt(CONTENT_LENGTH_WEIGHT_FILE, 0)
+    }
+
+    fun saveDraft(value: String) {
+        sharedPreferences!!.edit().putString(DIAGNOSTIC_DATA, value).apply()
+    }
+
+    fun clearDraft() {
+        sharedPreferences!!.edit().putString(DIAGNOSTIC_DATA, "{}").apply()
+    }
+
+    fun getDraftData(): String {
+        return sharedPreferences!!.getString(DIAGNOSTIC_DATA, "{}") ?: "{}"
+    }
+
+    fun setSdkVersion(versionNo: String) {
+        sharedPreferences!!.edit().putString(SDK_VERSION, versionNo).apply()
+    }
+
+    fun getSdkVersion(): String {
+        return sharedPreferences!!.getString(SDK_VERSION, "") ?: ""
+    }
+
+    fun setOSVersion(versionNo: String) {
+        sharedPreferences!!.edit().putString(OS_VERSION, versionNo).apply()
+    }
+
+    fun getOSVersion(): String {
+        return sharedPreferences!!.getString(OS_VERSION, "") ?: ""
+    }
+
+    companion object {
+        private var preference: ContoursPreference? = null
+        val SHOW_TIPS_INTRO_SCREEN = "INTRO_SCREEN_TIPS"
+        val SHOW_FOUR_CORNERS_INTRO_SCREEN = "INTRO_SCREEN_FOUR_CORNERS"
+        val SHOW_ENDORSE_INTRO_SCREEN = "INTRO_SCREEN_ENDORSE"
+        val AES_KEY = "AES_KEY"
+        val DEVICE_ID = "DEVICE_ID"
+        val VERSION_NO = "VERSION_NO"
+        val SDK_VERSION = "SDK_VERSION"
+        val OS_VERSION = "OS_VERSION"
+        val COUNT_OFFLINE_VALIDATION = "COUNT_OFFLINE_VALIDATION"
+        val CONTENT_LENGTH_WEIGHT_FILE = "CONTENT_LENGTH_WEIGHT_FILE"
+        val DIAGNOSTIC_DATA = "DIAGNOSTIC_DATA"
+
+        fun getInstance(context: Context): ContoursPreference {
+            if (preference == null) {
+                preference = ContoursPreference(context)
+            }
+            return preference!!
+        }
+    }
+
+    init {
+        sharedPreferences = context.getSharedPreferences(CONTOURS_PREFERENCES, Context.MODE_PRIVATE)
+    }
+}
---
+++ docscan/src/main/java/com/contourdocumentimaging/android/contours_ai/util/Utility.kt
@@ -0,0 +1,138 @@
+package com.contourdocumentimaging.android.contours_ai.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.PorterDuff
+import android.os.Build
+import android.util.TypedValue
+import android.view.View
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+
+class Utility {
+
+    companion object {
+
+
+
+        fun deviceType(context: Context): String {
+            return if(isTablet(context)) {
+                "Tablet"
+            } else {
+                "Phone"
+            }
+        }
+        private fun isTablet(context: Context): Boolean {
+            val xlarge = context.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK === 4
+            val large = context.resources.configuration.screenLayout and Configuration.SCREENLAYOUT_SIZE_MASK === Configuration.SCREENLAYOUT_SIZE_LARGE
+            return xlarge || large
+        }
+
+        fun convertDpToPixel(dp: Int): Int {
+            val metrics = Resources.getSystem().displayMetrics
+            val px = dp * (metrics.densityDpi / 160f)
+            return Math.round(px)
+        }
+
+        fun roundDecimal(d: Float): Double {
+            return Math.round(d * 100.0)/100.0
+        }
+
+        fun roundDecimal(d: Double): Double {
+            return Math.round(d * 100.0)/100.0
+        }
+
+        fun loadValueFromResource(context: Context, id: Int): Double {
+            val typedValue = TypedValue()
+            context.resources.getValue(id, typedValue, true)
+            return roundDecimal(typedValue.float)
+        }
+
+        /**
+         * calculating the openCV box percentage with compare to image resolution.
+         */
+        public fun calculatePercentage(value:Double, minValue:Double): Double{
+            val v = minValue / value
+            return v * 100.0
+        }
+
+        /**
+         * calculating the values of layer with openCV percentage
+         */
+        public fun calculateValueInPercentage(value:Double, percentageVal:Double): Double{
+            val v = value * percentageVal
+            return v / 100.0
+        }
+
+        /**
+         * This method is responsible for vibrating device. This method will be called when
+         * haptic feedback is given to the user.
+         * @param activity Activity instance.
+         */
+        fun vibrateDevice(activity: Activity, vibrateTime: Long = 200) {
+
+        }
+
+        /**
+         * To check color is valid or not
+         */
+        fun isColorValid(color: Int): Boolean {
+            return (color in 0..255)
+        }
+
+        fun changeCTAButtonBackgroundColor(ctaButton: View?, colorId: Int) {
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    ctaButton?.background?.colorFilter = BlendModeColorFilter(ContextCompat.getColor(ctaButton!!.context, colorId), BlendMode.SRC_ATOP)
+                } else {
+                    ctaButton?.background?.setColorFilter(ContextCompat.getColor(ctaButton.context, colorId), PorterDuff.Mode.SRC_ATOP)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        fun millisecondToSecond(millisecond: Long): Int {
+            return (millisecond/1000).toInt()
+        }
+
+        fun getCurrentTimeStamp(): Long {
+            return Date().time
+        }
+
+        fun getDateAndTime(): String {
+            val df: DateFormat = SimpleDateFormat("EEE, d MMM yyyy, HH:mm", Locale.getDefault())
+            return df.format(Calendar.getInstance().time)
+        }
+
+        fun deleteFileIfExist(context: Context, fileName: String) {
+            val file = File(context.filesDir, fileName)
+            if (file.exists()) {
+                file.delete()
+            }
+        }
+
+        /**
+         * Deletes the file if exists
+         */
+        fun deleteFileIfExist(file: File) {
+            if (file.exists()) {
+                file.delete()
+            }
+        }
+
+        private fun getMinimumSupportedOpenCVVersion(): String {
+            return "4.5.0"
+        }
+    }
+}
