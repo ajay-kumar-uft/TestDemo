4901547f3228a2b81ff84d20ad8a9521498c331b
Ajay Kumar <ajay.kumar@urbanft.com>
2024-01-18 19:10:05 +0530

    Config changes

--- docscan/src/main/java/com/document/android/contours/DocScan.kt
+++ docscan/src/main/java/com/document/android/contours/DocScan.kt
@@ -1,6 +1,5 @@
 package com.document.android.contours
 
-import androidx.annotation.Keep
 import com.document.android.contours.a.Scan
 import com.document.android.contours.b.CaptureHelper
 import com.document.android.contours.c.CScanHelper
@@ -9,20 +8,18 @@ import com.document.android.contours.scan.ScanHelper
 import com.document.android.contours.util.CheckCaptureHelper
 import com.document.android.contours.util.ContoursPreference
 
-public class DocScan {
+class DocScan {
 
-    companion object {
-        fun init() {
-            val isGood = CheckCaptureHelper().isGood
-            ScanHelper().isGood
-            Scan().isGood
-            CaptureHelper().isGood
-            CScanHelper().isGood
-            DScanHelper().isGood
-            ContoursPreference.Companion.VERSION_NO
-            ContoursStarterActivity().start()
-            ContoursCryptoUtils().start()
-        }
+    fun init() {
+        val isGood = CheckCaptureHelper().isGood
+        ScanHelper().isGood
+        Scan().isGood
+        CaptureHelper().isGood
+        CScanHelper().isGood
+        DScanHelper().isGood
+        ContoursPreference.Companion.VERSION_NO
+        ContoursStarterActivity().start()
+        ContoursCryptoUtils().start()
     }
 
 }
